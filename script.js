7262630c2dd43dfb69bf7c083b655d53c71f087e
williambayliss <wmbayliss@gmail.com>
2021-10-20 13:26:05 -0400

    Added comments

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+// HTML Elements / Document selectors
 const libraryContainer = document.getElementById('library-container');
 const newBookButton = document.getElementById('new-book-button');
 const newBookForm = document.getElementById('new-book-form')
 
+// Toggles display of the new book form in HTML
 newBookButton.addEventListener('click', function(event) {
     newBookForm.classList.toggle('hidden-form');
 });
 
+// Array of Book objects
 let myLibrary = [];
 
+// Object constructor for Books
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
+    // Will return a string with the properties of the book
     this.info = function() {
         if (read == true) {
             return `${title}, ${author}, ${pages} pages. You have read this book`;
@@ -24,11 +29,12 @@ function Book(title, author, pages, read) {
     };
 };
 
+// Takes a book and adds it to the myLibrary array
 function addBookToLibrary(book) {
-    //stuff
     myLibrary.push(book);
 };
 
+// For each book will call the displayBook function
 function displayLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
         let book = myLibrary[i]
@@ -41,6 +47,7 @@ function displayLibrary() {
     };
 };
 
+// Will create an HTML element for a Book object and append it to the libraryContainer
 function displayBook(book) {
 
 }
