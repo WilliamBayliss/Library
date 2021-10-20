38a9a00a05c3306135150f360d562d29de2b1dd3
williambayliss <wmbayliss@gmail.com>
2021-10-20 12:15:36 -0400

    Added a function to Book constructor to return a string of the book info. Addeda function to create an HTML element for each book that will display the title, added code to add a book to the library array, added code for testing at the bottom to remove later

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-let myLibrary = []
+let libraryContainer = document.getElementById('library-container');
 
-function Book(title, author, pages) {
-    title: title
-    author: author
-    pages: pages
-    read: false
-}
+let myLibrary = [];
 
-function addBookToLibrary() {
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        if (read == true) {
+            return `${title}, ${author}, ${pages} pages. You have read this book`;
+        }
+        else {
+            return `${title}, ${author}, ${pages} pages. You have not read this book`;
+        }
+    };
+};
+
+function addBookToLibrary(book) {
     //stuff
-}
+    myLibrary.push(book);
+};
+
+function displayLibrary() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i]
+        let bookDisplay = document.createElement('div');
+        bookDisplay.innerHTML = book.title;
+
+        libraryContainer.appendChild(bookDisplay);
+
+
+    };
+};
+
+let book = new Book("The Lord of the Rings", "JRR Tolkein", 455, true);
+addBookToLibrary(book);
+console.log(book.info())
+displayLibrary();
