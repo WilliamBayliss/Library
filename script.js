891a6b22e0007ad26d19e38cb54d509e7eee5f2c
williambayliss <wmbayliss@gmail.com>
2021-10-22 12:32:46 -0400

    Added cute little book symbol to book info header on book display

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ function displayBook(book, index) {
     
     // Create header for book info, set inner HTML and append to div
     let bookInfo = document.createElement('h5');
-    bookInfo.innerHTML = book.info();
+    bookInfo.innerHTML = "📖" + book.info();
     bookDisplay.appendChild(bookInfo);
 
     // Create delete button, set inner HTML, add button class and append to bookDisplay Div
